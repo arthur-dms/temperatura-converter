8059aaff996fb161a5a691663f88064c61a982e3
arthur-dms <arthurdouglas.135@gmail.com>
2026-05-24 17:17:16 -0400

    build quebrado

--- src/main/java/com/temp/TemperaturaConverter.java
+++ src/main/java/com/temp/TemperaturaConverter.java
@@ -4,6 +4,9 @@ public class TemperaturaConverter {
     public static double fahrenheitParaCelsius(double f) {
         return (f - 32) * 5.0 / 9.0;
     }
+
+SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS{}}}}{{{WWWWWWWWWWWW
+
     public static double celsiusParaFahrenheit(double c) {
         return c * 9.0 / 5.0 + 32;
     }
