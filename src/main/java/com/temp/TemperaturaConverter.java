247e65c51194335338321c294a48e1b92aeae29d
arthur-dms <arthurdouglas.135@gmail.com>
2026-05-24 17:23:34 -0400

    logica errada - testes falham

--- src/main/java/com/temp/TemperaturaConverter.java
+++ src/main/java/com/temp/TemperaturaConverter.java
@@ -2,10 +2,8 @@ package com.temp;
 
 public class TemperaturaConverter {
     public static double fahrenheitParaCelsius(double f) {
-        return (f - 32) * 5.0 / 9.0;
+        return (f - 32) * 500.0 / 9.0;
     }
-
-SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS{}}}}{{{WWWWWWWWWWWW
 
     public static double celsiusParaFahrenheit(double c) {
         return c * 9.0 / 5.0 + 32;
