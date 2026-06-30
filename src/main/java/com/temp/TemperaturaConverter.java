5d13ea6f56beb01abc36018498ac69cb2a247eb1
arthur-dms <arthurdouglas.135@gmail.com>
2026-06-29 23:27:07 -0400

    erro de compilação

--- src/main/java/com/temp/TemperaturaConverter.java
+++ src/main/java/com/temp/TemperaturaConverter.java
@@ -3,7 +3,7 @@ package com.temp;
 public class TemperaturaConverter {
     public static double fahrenheitParaCelsius(double f) {
         return (f - 32) * 5.0 / 9.0;
-    }
+    } BRASIL METEU NOS JAPA VAI BRAZILLIANNNNNNNNNNNNNNNNNNNNNNNNNNNNN
 
     public static double celsiusParaFahrenheit(double c) {
         return c * 9.0 / 5.0 + 32;
